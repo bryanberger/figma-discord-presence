e8a82e28c8cb9a897370de229682d6e7e7d51551
Bryan Berger <bryan@bryanberger.com>
2021-08-02 21:58:52 -0400

    feat: added sentry for crash/error analytics

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,8 +1,19 @@
+const Sentry = require("@sentry/electron");
+const { CaptureConsole } = require("@sentry/integrations");
 const log = require("electron-log");
 
 log.transports.console.format = "[{h}:{i}:{s}.{ms}] {text}";
 log.transports.file.format = "[{y}-{m}-{d} {h}:{i}:{s}.{ms}] {text}";
 log.transports.file.level = "debug";
+
+Sentry.init({
+  dsn: "https://b32c5d25554f4e6ebed361104462766a@o940691.ingest.sentry.io/5890015",
+  integrations: [
+    new CaptureConsole({
+      levels: ["error"],
+    }),
+  ],
+});
 
 module.exports = {
   info: function (tag, msg) {
